a1fc48c05f98ecc4efa381eafd3434a22a162e28
AaadiSathe <96834038+AaadiSathe@users.noreply.github.com>
2024-12-19 17:15:46 +0530

    Update vite.config.ts

--- React-Demo/vite.config.ts
+++ React-Demo/vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/sqlbe': {
+        target: 'http://13.233.232.244:8080', // your backend server URL
+        changeOrigin: true,  // To avoid issues with origin
+        rewrite: (path) => path.replace(/^\/sqlbe/, ''), // Remove `/sqlbe` prefix
+      },
+    },
+  },
 })
